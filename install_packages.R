bfb0b94d0622003ec3e4d227fb002856f9b7bba4
Vincent Nijs <vnijs@ucsd.edu>
2026-01-21 21:05:13 -0800

    back to cran

--- install_packages.R
+++ install_packages.R
@@ -1,6 +1,6 @@
 # Set options
 options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version$platform, R.version$arch, R.version$os)))
-repos <- c(CRAN = "https://packagemanager.posit.co/cran/2025-12-01", RSM="https://radiant-rstats.github.io/minicran")
+repos <- c(CRAN = "https://cloud.r-project.org")
 options(repos = repos)
 
 # Determine package type based on OS
@@ -17,23 +17,12 @@ install <- function(x) {
   pkgs <- x[!x %in% ipkgs]
   if (length(pkgs) > 0) {
     cat(paste("   Installing:", paste(pkgs, collapse = ", "), "\n"))
-    install.packages(pkgs, lib = .libPaths()[1], type = os_type, repos = "https://packagemanager.posit.co/cran/2025-12-01")
+    install.packages(pkgs, lib = .libPaths()[1], type = os_type)
   }
 }
 
 # Install core packages
-install.packages("xgboost", lib = .libPaths()[1], type = os_type, repos = "https://cloud.r-project.org/")
 install(c("radiant", "miniUI", "webshot", "usethis", "remotes", "tinytex"))
-
-ipkgs <- rownames(installed.packages())
-install <- function(x) {
-  pkgs <- x[!x %in% ipkgs]
-  if (length(pkgs) > 0) {
-    install.packages(pkgs, lib = .libPaths()[1], repos = "https://packagemanager.posit.co/cran/2025-12-01")
-  }
-}
-
-install(c("radiant", "radiant.model", "radiant.multivariate", "radiant.data", "radiant.basics", "radiant.design"))
 
 # Install PhantomJS for webshot
 cat("Installing PhantomJS for screenshots...\n")

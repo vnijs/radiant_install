329405fa42a9f357553735340bbf3292c37ffa2d
Vincent Nijs <vnijs@ucsd.edu>
2026-01-25 10:19:28 -0800

    install pdp directly from cran

--- install_packages.R
+++ install_packages.R
@@ -22,7 +22,7 @@ install <- function(x) {
 }
 
 # Install core packages
-install(c("radiant", "miniUI", "webshot", "usethis", "remotes", "tinytex"))
+install(c("radiant", "miniUI", "webshot", "usethis", "remotes", "tinytex", "pdp"))
 
 # Install PhantomJS for webshot
 cat("Installing PhantomJS for screenshots...\n")
